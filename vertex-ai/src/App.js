891e8daec4cefe9e88da5fb444a8855b6f0da2c9
Luis Martins <luiss_martinss_21@hotmail.com>
2024-11-09 12:22:42 +0000

    WebSite Start

--- vertex-ai/src/App.js
+++ vertex-ai/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Vertex AI
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/LuisMartins21/VERTEX.ai"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Git Repository
-        </a>
-      </header>
-    </div>
-  );
+        return (
+                <div className="App">
+                        <nav className="App-nav">
+                                <ul>
+                                        <li>
+                                                <a
+                                                        className="App-item1"
+                                                        href=""
+                                                >
+                                                        Home
+                                                </a>
+                                        </li>
+                                        <li>
+                                                <a
+                                                        className="App-item2"
+                                                        href=""
+                                                >
+                                                        Services
+                                                </a>
+                                        </li>
+                                        <li>
+                                                <a
+                                                        className="App-item3"
+                                                        href=""
+                                                >
+                                                        About
+                                                </a>
+                                        </li>
+                                        <li>
+                                                <a
+                                                        className="App-item4"
+                                                        href=""
+                                                >
+                                                        Chat
+                                                </a>
+                                        </li>
+                                </ul>
+                        </nav>
+                        <header className="App-header">
+                                <p>VERTEX.AI</p>
+                                <a
+                                        className="App-link"
+                                        href="https://github.com/LuisMartins21/VERTEX.ai"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                >
+                                        Git Repository
+                                </a>
+                        </header>
+                </div>
+        );
 }
 
 export default App;
