7eaf0299ca224123d286db3b146c0a5c41d62c2f
Luis Martins <luiss_martinss_21@hotmail.com>
2024-12-25 12:04:13 +0000

    Register System Added (UNTESTED)

--- server/index.js
+++ server/index.js
@@ -1,23 +1,35 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-        "mongodb+srv://luissmartinss:LlUuIiSs212526@vertexai.pnavd.mongodb.net/?retryWrites=true&w=majority&appName=vertexai";
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const User = require("../vertex-ai/src/components/signinform.jsx");
 
-const client = new MongoClient(uri, {
-        serverApi: {
-                version: ServerApiVersion.v1,
-                strict: true,
-                deprecationErrors: true,
-        },
+const app = express();
+app.use(express.json());
+
+mongoose.connect(
+        "mongodb+srv://luissmartinss:LlUuIiSs212526@vertexai.pnavd.mongodb.net/?retryWrites=true&w=majority&appName=vertexai",
+        { userNewUrlParser: true, useUniFiedTopology: true },
+);
+
+app.post("/signin", async (req, res) => {
+        const { usename, email, password } = req.body;
+
+        const existingUser = await User.findOne({ username });
+        if (existingUser) {
+                return res
+                        .status(400)
+                        .json({ message: "Sorry! Username Already Taken" });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+
+        res.status(201).json({
+                message: "Congratulations! Your Account Was Created!",
+        });
 });
 
-async function run() {
-        try {
-                await client.connect();
-
-                await client.db("admin").command({ ping: 1 });
-                console.log("Welcome to VertexAI DB");
-        } finally {
-                await client.close();
-        }
-}
-run().catch(console.dir);
+app.listen(3000, () => {
+        console.log("Server Running On 3000");
+});
--- vertex-ai/src/components/signinform.jsx
+++ vertex-ai/src/components/signinform.jsx
@@ -1,17 +1,29 @@
 import "./signinform.css";
 
+//Back-End
+/*const form = document.getElementById("Signin-Form");
+form.addEventListener("submit", registerUser);
+
+function registerUser(event) {
+        event.preventDefault();
+        const username = document.getElementById("username");
+        const email = document.getElementById("email");
+        const password = document.getElementById("password");
+        }*/
+
+//Front-End
 export default function SigninForm() {
         return (
                 <div className="Signin-Form">
                         <form>
                                 <p>Welcome to Vertex.AI</p>
                                 <div className="Input-Area">
-                                        <label for="name">Your Name:</label>
+                                        <label for="username">Your Name:</label>
                                         <input
                                                 placeholder="Name"
                                                 type="text"
-                                                id="name"
-                                                name="name"
+                                                id="username"
+                                                name="username"
                                         />
                                         <label for="email">Your Email:</label>
                                         <input
