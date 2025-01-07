26c16b11b70bdb003cf94cf8d3bc2ab34f0ee840
Luis Martins <luiss_martinss_21@hotmail.com>
2025-01-07 10:17:09 +0000

    ....

--- server/index.js
+++ server/index.js
@@ -3,14 +3,14 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const User = require("./userModel");
 const cors = require("cors");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 
-require('dotenv').config()
+require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI);
@@ -31,33 +31,41 @@ app.post("/signin", async (req, res) => {
 
         res.cookie("username", newUser.username, {
                 httpOnly: true,
-                secure: false, 
+                secure: false,
                 sameSite: "lax",
-            });
+        });
 
         res.status(201).json({
                 message: "Congratulations! Your Account Was Created!",
         });
 });
 
-app.post("/signup", async(req, res) => {
-        const {email, password} = req.body;
-        const existingUser = await User.findOne({email});0
-        if(!existingUser){
-            return res.status(400).json({message: "User account doesn't exists..."})    
+app.post("/signup", async (req, res) => {
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        0;
+        if (!existingUser) {
+                return res
+                        .status(400)
+                        .json({ message: "User account doesn't exists..." });
         } else {
-                const isMatch = await bcrypt.compare(password, existingUser.password);
-                if(!isMatch){
-                    return res.status(400).json({message: "Invalid credentials..."})
+                const isMatch = await bcrypt.compare(
+                        password,
+                        existingUser.password,
+                );
+                if (!isMatch) {
+                        return res
+                                .status(400)
+                                .json({ message: "Invalid credentials..." });
                 }
                 res.cookie("username", existingUser.username, {
                         httpOnly: true,
                         secure: false,
                         sameSite: "lax",
-                    });
-                res.json({message: "Logged in successfully!"})
+                });
+                res.json({ message: "Logged in successfully!" });
         }
-})
+});
 
 app.post("/logout", (req, res) => {
         res.clearCookie("username");
@@ -66,14 +74,14 @@ app.post("/logout", (req, res) => {
 
 app.get("/auth/check", (req, res) => {
         const { username } = req.cookies;
-    
-        if (!username) {
-            return res.status(401).json({ message: "Not authenticated" });
-        }
-    
-        res.json({ username });
-    });
 
-app.listen(5000, () => {
-        console.log("Server Running On 5000");
+        if (!username) {
+                return res.status(401).json({ message: "Not authenticated" });
+        }
+
+        res.json({ username });
+});
+
+app.listen(8000, () => {
+        console.log("Server Running On 8000");
 });
